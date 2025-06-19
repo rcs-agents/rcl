707eabee82a08d4b7867910f86c83283afbd6703
Saulo Vallory <me@saulo.engineer>
2025-06-19 19:01:30 -0300

    fix(extension): correct language server file extension from .mjs to .cjs

--- packages/extension/src/extension/main.ts
+++ packages/extension/src/extension/main.ts
@@ -19,7 +19,7 @@ export function deactivate(): Thenable<void> | undefined {
 }
 
 async function startLanguageClient(context: vscode.ExtensionContext): Promise<LanguageClient> {
-  const serverModule = context.asAbsolutePath(path.join('out', 'language', 'main.mjs'));
+  const serverModule = context.asAbsolutePath(path.join('out', 'language', 'main.cjs'));
   // The debug options for the server
   // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging.
   // By setting `process.env.DEBUG_BREAK` to a truthy value, the language server will wait until a debugger is attached.
